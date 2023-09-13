f0e4bcac21d34d1eb94e455bc99b7b143fcb84df
Cornell Reddick <cornellreddick@outlook.com>
2023-09-12 23:14:51 -0400

    Created Basic Tool Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './conponent/toolbar/toolbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
