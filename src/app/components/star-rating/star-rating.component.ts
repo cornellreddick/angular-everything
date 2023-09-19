98a15a4f92be50fd9edbae22baea95ee348668a7
Cornell Reddick <cornellreddick@outlook.com>
2023-09-19 17:41:57 -0400

    Completed Star Component rating.

--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -14,4 +14,19 @@ export class StarRatingComponent {
     return Array(totalFullStars).fill(0);
   }
 
+  public get hasHalfStar(): boolean{
+    const highestRating = 5;
+    const hasHalfStar = (this.rating - Math.floor(this.rating) >= 0.5) && this.rating !== 5;
+
+    return hasHalfStar;
+  }
+
+  public get emptyStars(): number[] {
+    const highestRating = 5;
+    const totalEmptyStars = Math.floor(highestRating - this.rating)
+  
+
+    return Array(totalEmptyStars).fill(0);
+  }
+
 }
