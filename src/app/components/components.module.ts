de855ae967a10df5b75bf26618e01be181e7ea79
cornellreddick <cornellreddick@outlook.com>
2023-10-09 12:39:49 -0400

    Create loading component
    Signed-off-by: cornellreddick <cornellreddick@outlook.com>

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ComponentDocumentationComponent } from './component-documentation/component-documentation.component';
@@ -8,6 +7,7 @@ import { AccordionComponent } from './accordion/accordion.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { SharedModule } from './shared/shared.module';
 import { StarRatingComponent } from './star-rating/star-rating.component';
+import { LoaderComponent } from './loader/loader.component';
 
 
 @NgModule({
--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
 import { BannerCutOutComponent } from '../banner-cut-out/banner-cut-out.component';
+import { LoaderComponent } from '../loader/loader.component';
 
 @NgModule({
   declarations: [
     CardComponent,
-    BannerCutOutComponent
+    BannerCutOutComponent,
+    LoaderComponent
   ],
   exports: [
     CardComponent,
-    BannerCutOutComponent
+    BannerCutOutComponent,
+    LoaderComponent
 
   ],
   imports: [
